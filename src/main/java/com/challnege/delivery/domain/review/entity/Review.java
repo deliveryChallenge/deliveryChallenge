29d3f60e6d9f8d62c4ba70570c116436a38aafe5
DoKkangs <snnam0421@naver.com>
2024-01-12 21:54:42 +0900

    be/dev merge

---
+++ src/main/java/com/challnege/delivery/domain/review/entity/Review.java
@@ -0,0 +1,37 @@
+package com.challnege.delivery.domain.review.entity;
+
+import com.challnege.delivery.domain.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+
+@Getter
+@Setter
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long reviewId;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    private Restaurant restaurant;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @Column(nullable = false)
+    private int rating;
+
+}
